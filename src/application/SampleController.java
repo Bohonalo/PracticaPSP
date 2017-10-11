79be41e2a378397fd63100a37aa37f3b4348b953
21639999 <21639999@N209-06.alumnos.uem.es>
2017-10-11 10:07:27 +0200

    1.0

---
+++ src/application/SampleController.java
@@ -0,0 +1,28 @@
+package application;
+
+	import com.jfoenix.controls.JFXButton;
+	import com.jfoenix.controls.JFXTextArea;
+	import com.jfoenix.controls.JFXTextField;
+	import javafx.fxml.FXML;
+
+	public class SampleController {
+
+	    @FXML
+	    private JFXButton btnExcel;
+
+	    @FXML
+	    private JFXButton btnWord;
+
+	    @FXML
+	    private JFXButton btnPower;
+
+	    @FXML
+	    private JFXTextField textNavegar;
+
+	    @FXML
+	    private JFXTextArea textArea;
+
+	    @FXML
+	    private JFXButton btnNavegar;
+
+}
