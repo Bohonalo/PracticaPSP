d2f39673820c88f26c2221bd56d026921aa8cda0
Bohonalo <bohonalo@gmail.com>
2017-10-17 17:39:11 +0200

    con la tabla cargada

--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -1,15 +1,37 @@
 package application;
 
-	import com.jfoenix.controls.JFXButton;
-	import com.jfoenix.controls.JFXTextArea;
-	import com.jfoenix.controls.JFXTextField;
-	import javafx.fxml.FXML;
+import java.io.*;
+import java.util.ArrayList;
+
+import com.jfoenix.controls.*;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 
 	public class SampleController {
-
+		
+		String nombre = "urls.txt";
+		BufferedWriter bw = null;
+		
+		BufferedReader br = null;
+		
 	    @FXML
 	    private JFXButton btnExcel;
+	    
+	    @FXML
+	    private TableView<StringProperty> tablaWeb;
+	    @FXML
+	    private TableColumn<StringProperty, String> colum;
 
 	    @FXML
 	    private JFXButton btnWord;
@@ -21,30 +43,111 @@ import javafx.scene.input.MouseEvent;
 	    private JFXTextField textNavegar;
 
 	    @FXML
-	    private JFXTextArea textArea;
-
-	    @FXML
 	    private JFXButton btnNavegar;
 	    
 	    @FXML
+	    private JFXTextArea textArea;
+	    
+	    private main main;
+	    
+	    
+	    	    
+	    @FXML
 	    void excel(MouseEvent event) {
-	    	
+	    	try {
+				new ProcessBuilder("C:\\Program Files\\Microsoft Office\\Office16\\EXCEL.exe").start();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 	    }
 	    
 	    @FXML
 	    void word(MouseEvent event) {
-	    	
+	    	try {
+				new ProcessBuilder("C:\\Program Files\\Microsoft Office\\Office16\\WINWORD.exe").start();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 	    }
 	    
 	    @FXML
 	    void Power(MouseEvent event) {
-	    	
+	    	try {
+	    		
+				new ProcessBuilder("C:\\Program Files\\Microsoft Office\\Office16\\POWERPNT.exe").start();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 	    }
 	    
 	    @FXML
 	    void navegar(MouseEvent event) {
+	    	  try {
+				Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + textNavegar.getText());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+	    	escribirUrls();
+	    	leerUrls();
 	    	
+		  
+
+	    }
+	    
+		private void leerUrls() {
+			
+			for (int i = 0; i< main.getArrayUrls().size(); i++) {
+				textArea.setText(textArea.getText() + "\n" + main.getArrayUrls().get(i));	
+			}
+			for (int i = 0; i< main.getArrayUrls().size(); i++) {
+			
+			}
+		  
+		}
+		
+		 @FXML
+	    private void initialize() {
+	        // Initialize the table with the one column.
+			 colum.setCellValueFactory(
+	                cellData -> cellData.getValue());
+
+	        //TODO: visualizar la web selecionada
+
+	        // Listen for selection changes and show the person details when changed.
+	        /*tablaWeb.getSelectionModel().selectedItemProperty().addListener(
+	                (observable, oldValue, newValue) -> visualizrUrl(newValue));*/
 	    }
 
+		private void escribirUrls() {	    	  
+	    	  try { 
+				bw = new BufferedWriter(new FileWriter(nombre,true));
+				for (int i = 0; i < main.getArrayUrls().size(); i++) {
+					if(!main.getArrayUrls().contains(textNavegar.getText())){
+						bw.write(textNavegar.getText());
+						bw.newLine();
+						main.getArrayUrls().add(new SimpleStringProperty(textNavegar.getText()));
+					}
+				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}finally {
+				if (bw != null) {
+					try {
+						bw.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+
+		public void inicializarTabla(main main) {
+			this.main = main;
+
+	        // Add observable list data to the table
+	        tablaWeb.setItems(main.getArrayUrls());
+			
+		}
 
 }
---
+++ src/application/main.java
@@ -0,0 +1,90 @@
+package application;
+	
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.fxml.FXMLLoader;
+
+
+public class main extends Application {
+	private AnchorPane root;
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			//AnchorPane root = (AnchorPane)loader.load(getClass().getResource("Sample.fxml"));
+			
+			 loader.setLocation(main.class.getResource("Sample.fxml"));
+	         root = (AnchorPane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(root);
+			
+			//Scene scene = new Scene(root,600,400);
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+			SampleController controller = loader.getController();
+			leerUrls();
+            controller.inicializarTabla(this);
+
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	//public 	ObservableList<String> arrayUrls = FXCollections.observableArrayList();
+	private ObservableList<StringProperty> arrayUrls = FXCollections.observableArrayList();
+	
+	public void leerUrls() {
+	
+		String nombre = "urls.txt";
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(nombre));
+			
+			StringProperty linea = null;
+			while((linea = new SimpleStringProperty(br.readLine())).getValue() !=  null){
+				arrayUrls.add(linea);	
+		      }
+		} catch (FileNotFoundException e) {
+			System.out.println("Fichero no ha sido encontrado");
+		} catch (IOException e) {
+			System.out.println("Error IO: " + e.getMessage());
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
+	
+	
+	public ObservableList<StringProperty> getArrayUrls() {
+		return arrayUrls;
+	}
+
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+}
